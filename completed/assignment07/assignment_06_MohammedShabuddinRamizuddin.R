09ad1d697adbcb2b46508d9a835302d327762ee6
RMohammedShabuddin <62000492+RMohammedShabuddin@users.noreply.github.com>
2020-07-12 13:41:34 -0500

    assignment 6

---
+++ completed/assignment07/assignment_06_MohammedShabuddinRamizuddin.R
@@ -0,0 +1,14 @@
+---
+title: "6.1 Assignment Housing Data"
+author: "RamizuddinMohammedShabuddin"
+date: "7/09/2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+
+setwd("E:/Bellevue/Semester3Assignment/dsc520/data")
+Housing_Data  <- read_excel("week-7-housing.xlsx")
